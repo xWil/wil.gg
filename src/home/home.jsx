8e2facc67b1fb8cc385537f42761321344bc62c5
xWil <pengugames81@gmail.com>
2025-09-11 23:53:46 +0100

    + Switched to react and remade home page

---
+++ src/home/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './home.css'
+
+function home() {
+    return (
+        <div className="app">
+            <div className="star-container">
+                <div className="stars"></div>
+                <h1 className="title">wil.gg</h1>
+            </div>
+        </div>
+
+    )
+}
+
+export default home
