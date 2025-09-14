fdb40d150abb14e9ab18ee903c66ac7f276f87b1
xWil <pengugames81@gmail.com>
2025-09-14 17:23:52 +0100

    + Added details to the current project card

--- src/home/current-project/current-project.jsx
+++ src/home/current-project/current-project.jsx
@@ -4,6 +4,11 @@ import styles from './current-project.css'
 function currentProject() {
     return (
         <div className="project-card">
+            <img className="project-image" src="/assets/current-project.png" alt="project screenshot"/>
+            <div className="project-info">
+                <h1 className="project-title">VLRScraper</h1>
+                <h2 className="project-description">A web scraper and data analysis tool for Valorant esports game data found on vlr.gg</h2>
+            </div>
         </div>
     )
 }
