571558dfe2d26533174806db80e01a17b1e0c34f
xWil <pengugames81@gmail.com>
2025-09-14 03:06:06 +0100

    + Added about me card and current project card

---
+++ src/home/current-project/current-project.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './current-project.css'
+
+function currentProject() {
+    return (
+        <div className="project-card">
+        </div>
+    )
+}
+
+export default currentProject;
--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import styles from './home.css'
 
+import ProfileCard from './profile-card/profile-card.jsx'
+import CurrentProject from "./current-project/current-project";
+
 function home() {
     return (
         <div className="app">
             <div className="star-container">
                 <div className="stars"></div>
-                <h1 className="title">wil.gg</h1>
+                <div className="title-container">
+                    <h1 className="title">wil.gg</h1>
+                    <h2 className="subtitle">my portfolio</h2>
+                </div>
+            </div>
+            <div className="profile-container">
+                <div className="card-container">
+                    <h1 className="card-title">about me</h1>
+                    <ProfileCard/>
+                </div>
+                <div className="card-container">
+                    <h1 className="card-title">current project</h1>
+                    <CurrentProject/>
+                </div>
             </div>
         </div>
-
     )
 }
 
