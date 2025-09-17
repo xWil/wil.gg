25f4a13e1eb7d5dfd3b5d08cede2bfee2228323e
xWil <pengugames81@gmail.com>
2025-09-17 02:58:49 +0100

    + Moved links to bottom of profile + Adjusted font weights

--- src/home/profile-card/profile-card.jsx
+++ src/home/profile-card/profile-card.jsx
@@ -6,18 +6,16 @@ function profileCard() {
         <div className="profile-card">
             <img className="profile-picture" src="/assets/pfp.gif" alt="profile"/>
             <div className="profile-info">
-                <div className="profile-topline">
-                    <h1 className="profile-title">Hiya, I'm wil</h1>
-                    <div className="profile-links">
-                        <a href="https://github.com/xWil" target="_blank" rel="noreferrer">
-                            <img className="profile-link" src="/assets/github-icon.svg" alt="github"/>
-                        </a>
-                        <a href="https://discord.com/users/774392801521041439" target="_blank" rel="noreferrer">
-                            <img className="profile-link" src="/assets/discord-icon.svg" alt="discord"/>
-                        </a>
-                    </div>
-                </div>
+                <h1 className="profile-title">Hiya, I'm wil</h1>
                 <h2 className="profile-description">I'm a java dev based in the United Kingdom working a few different personal projects.</h2>
+                <div className="profile-links">
+                    <a href="https://github.com/xWil" target="_blank" rel="noreferrer">
+                        <img className="profile-link" src="/assets/github-icon.svg" alt="github"/>
+                    </a>
+                    <a href="https://discord.com/users/774392801521041439" target="_blank" rel="noreferrer">
+                        <img className="profile-link" src="/assets/discord-icon.svg" alt="discord"/>
+                    </a>
+                </div>
             </div>
         </div>
     )
